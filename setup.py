cb80eedaa7c2da8c497570e98d7b6cef59d8c8c2
Sergei Silnov <po@kumekay.com>
2018-10-09 11:36:08 +0200

    Add tox for running tests against different python versions

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="component_manager",
+    version="0.0.1",
+    author="Sergei Silnov",
+    author_email="sergei.silnov@espressif.com",
+    description="Component Manager for ESP IDF",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.espressif.cn:6688/sergei.silnov/component-manager",
+    packages=setuptools.find_packages(),
+    install_requires=['requests']
+)
