528b1707d31920a6776a2b4f3e0e0fba6b4f49e5
Sergei Silnov <sergei.silnov@espressif.com>
2020-06-24 17:41:42 +0200

    Add config manager

--- fixtures/components/cmp/include/cmp.h
+++
@@ -1,1 +0,0 @@
-void cmp_hello();
---
+++ tests/test_config.py
@@ -0,0 +1,76 @@
+import json
+import os
+import shutil
+import tempfile
+from io import open
+
+from idf_component_manager.config import Config, ConfigError, ConfigManager
+from pytest import raises
+
+
+def test_config_validation():
+    assert Config({}).validate()
+    assert Config(
+        {
+            'services': {
+                'default': {
+                    'url': 'default',
+                },
+                'in_office': {
+                    'url': 'http://api.localserver.local:5000/',
+                    'api_token': 'asdf',
+                    'default_namespace': 'asdf',
+                }
+            }
+        }).validate()
+
+    with raises(ConfigError):
+        Config('asdf').validate()
+
+    with raises(ConfigError):
+        Config({
+            'services': {
+                'in_office': {
+                    'url': 'pptp://api.localserver.local:5000/'
+                },
+            }
+        }).validate()
+
+
+def test_load_config():
+    tempdir = tempfile.mkdtemp()
+    config_path = os.path.join(tempdir, 'idf_component_manager.yml')
+    config = Config(
+        {
+            'services': {
+                'default': {
+                    'url': 'default',
+                },
+                'in_office': {
+                    'url': 'http://api.localserver.local:5000/',
+                    'api_token': 'asdf',
+                    'default_namespace': 'asdf',
+                }
+            }
+        })
+
+    # Use json representation to compare equality of nested dictionaries
+    config_json = json.dumps(dict(config), sort_keys=True, indent=2)
+
+    try:
+
+        manager = ConfigManager(path=config_path)
+        # save to file
+        manager.dump(config)
+
+        with open(config_path, mode='r', encoding='utf-8') as file:
+            assert file.readline().startswith('services:')
+
+        # load from file
+        loaded_config = manager.load()
+
+        assert loaded_config.services['in_office']['default_namespace'] == 'asdf'
+        assert config_json == json.dumps(dict(loaded_config), sort_keys=True, indent=2)
+
+    finally:
+        shutil.rmtree(tempdir)
