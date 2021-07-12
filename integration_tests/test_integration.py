cef701a4fb5c8a29104728c96bbcde6634ef29e6
Fu Hanxi <fuhanxi@espressif.com>
2021-07-12 17:46:43 +0800

    test: enable idf 4.1 4.2 integration tests

--- integration_tests/test_integration.py
+++ integration_tests/test_integration.py
@@ -1,10 +1,27 @@
+import logging
 import subprocess
 
 import pytest
 
 
 def build_project(project_path):
-    return subprocess.check_output(['idf.py', '-C', project_path, 'build'])
+    process = subprocess.Popen(
+        ['idf.py', '-C', project_path, 'build'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+    try:
+        string_type = basestring  # type: ignore
+    except NameError:
+        string_type = str
+
+    for line in process.stdout:
+        if not isinstance(line, string_type):
+            line = line.decode('utf-8')
+        logging.info(str(line.rstrip()))
+
+        if 'Project build complete.' in line:
+            return True
+
+    return False
 
 
 @pytest.mark.parametrize(
@@ -14,6 +31,7 @@ def build_project(project_path):
                 'main': {
                     'dependencies': {
                         'unity': {
+                            'version': '935deb4082676b42c66b1d1acb8278454bc77410',
                             'git': 'https://github.com/espressif/esp-idf.git',
                             'path': 'components/unity/',
                             'include': 'unity.h'
@@ -55,5 +73,4 @@ def build_project(project_path):
     ],
     indirect=True)
 def test_single_dependency(project):
-    build_output = build_project(project)
-    assert 'Project build complete.' in str(build_output)
+    assert build_project(project)
