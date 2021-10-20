7dbc0015545871cb939fb18aa3d508d71af5e9f7
Sergei Silnov <sergei.silnov@espressif.com>
2021-10-20 17:52:32 +0200

    Limit cffi version for py<3.6

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ SHORT_DESCRIPTION = 'The component manager for ESP-IDF'
 LICENSE = 'Apache License 2.0'
 URL = 'https://github.com/espressif/idf-component-manager'
 REQUIRES = [
+    'cffi<1.15;python_version<"3.6"',
     'future',
     'pathlib;python_version<"3.4"',
     'pyyaml',
