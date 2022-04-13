40e6f2d1dc34cbb77ed162b919c362751db96224
Dmitry Bondarenko <dmitry.bondarenko@espressif.com>
2022-04-13 15:13:33 +0300

    Fix bug when dependency has no version, and it was a validation error

--- idf_component_tools/manifest/validator.py
+++ idf_component_tools/manifest/validator.py
@@ -169,7 +169,7 @@ class ManifestValidator(object):
                 try:
                     source = tools.sources.BaseSource.fromdict(component, details)
 
-                    if not source.validate_version_spec(str(details.get('version'))):
+                    if not source.validate_version_spec(str(details.get('version', ''))):
                         self.add_error('Version specifications for "%s" are invalid.' % component)
                 except SourceError as unknown_keys_error:
                     self.add_error(str(unknown_keys_error))
