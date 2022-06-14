espressif/idf-component-manager

commit 6d09618498571dc958f745edb6706ea13eea1f1c

fix: support revision number in SimpleSpec

idf_component_tools/semver/base.py
@@ -134,6 +134,7 @@ class Version(object):
                 major=self.major,
                 minor=self.minor,
                 patch=self.patch,
+                revision=self.revision,
                 prerelease=self.prerelease,
             )
         elif level == 'patch':
@@ -814,6 +815,7 @@ class SimpleSpec(BaseSpec):
             r"""^
             (?P<op><|<=||=|==|>=|>|!=|\^|~|~=)
             (?P<major>{nb})(?:\.(?P<minor>{nb})(?:\.(?P<patch>{nb}))?)?
+            (?:~(?P<revision>\d+))?
             (?:-(?P<prerel>[a-z0-9A-Z.-]*))?
             (?:\+(?P<build>[a-z0-9A-Z.-]*))?
             $
@@ -853,12 +855,19 @@ class SimpleSpec(BaseSpec):
         def parse_block(cls, expr):
             if not cls.NAIVE_SPEC.match(expr):
                 raise ValueError('Invalid simple spec component: %r' % expr)
-            prefix, major_t, minor_t, patch_t, prerel, build = cls.NAIVE_SPEC.match(expr).groups()
+            prefix, major_t, minor_t, patch_t, revision, prerel, build = cls.NAIVE_SPEC.match(expr).groups()
             prefix = cls.PREFIX_ALIASES.get(prefix, prefix)
 
             major = None if major_t in cls.EMPTY_VALUES else int(major_t)
             minor = None if minor_t in cls.EMPTY_VALUES else int(minor_t)
             patch = None if patch_t in cls.EMPTY_VALUES else int(patch_t)
+            revision = None if revision in cls.EMPTY_VALUES else int(revision)
+
+            if (major is None or minor is None or patch is None) and (revision or prerel or build):
+                raise ValueError('Invalid simple spec: %r' % expr)
+
+            if build is not None and prefix not in (cls.PREFIX_EQ, cls.PREFIX_NEQ):
+                raise ValueError('Invalid simple spec: %r' % expr)
 
             if major is None:  # '*'
                 target = Version(major=0, minor=0, patch=0)
@@ -873,15 +882,10 @@ class SimpleSpec(BaseSpec):
                     major=major,
                     minor=minor,
                     patch=patch,
+                    revision=revision,
                     prerelease=prerel.split('.') if prerel else (),
                     build=build.split('.') if build else (),
                 )
-
-            if (major is None or minor is None or patch is None) and (prerel or build):
-                raise ValueError('Invalid simple spec: %r' % expr)
-
-            if build is not None and prefix not in (cls.PREFIX_EQ, cls.PREFIX_NEQ):
-                raise ValueError('Invalid simple spec: %r' % expr)
 
             if prefix == cls.PREFIX_CARET:
                 # Accept anything with the same most-significant digit

integration_tests/test_integration.py
@@ -83,7 +83,7 @@ def skip_for_idf_versions(*versions):
                 'main': {
                     'dependencies': {
                         'example/cmp': {
-                            'version': '^3.3.0',
+                            'version': '^3.3.0~0',
                             'include': 'cmp.h'
                         }
                     }

tests/conftest.py
@@ -8,13 +8,13 @@ from idf_component_tools.hash_tools import HASH_FILENAME
 
 @pytest.fixture()
 def valid_manifest_hash():
-    return '0f65559235d5ed5f9ab883549323b38e627f767cdfb5247f72988efb456bce67'
+    return '07c20149ed1f85db5660358da9aaf8b12715197f8373cca6e2f28b6ebc9eb2c8'
 
 
 @pytest.fixture()
 def valid_manifest():
     return {
-        'version': '2.3.1',
+        'version': '2.3.1~2',
         'targets': ['esp32'],
         'maintainers': ['Test Tester <test@example.com>'],
         'description': 'Test project',
@@ -26,7 +26,7 @@ def valid_manifest():
         'dependencies': {
             'idf': '~4.4.4',
             'test': {
-                'version': '>=8.2.0,<9.0.0'
+                'version': '>=8.2.0~1,<9.0.0'
             },
             'test-1': '^1.2.7',
             'test-8': {

tests/semver/test_match.py
@@ -23,6 +23,9 @@ class MatchTestCase(unittest.TestCase):
         '<=0.1.4a',
         '>0.1.1.1',
         '<0.1.2-rc1.3-14.15+build.2012-01-01.11h34',
+        '==0.0~2',
+        '>0.1~2',
+        '~=1~1',
     ]
 
     valid_specs = [
@@ -52,10 +55,15 @@ class MatchTestCase(unittest.TestCase):
             '0.1.2+build42-12.2012-01-01.12h23',
             '0.1.2-rc1.3-14.15+build.2012-01-01.11h34',
             '0.2.0',
+            '1.0.0~2',
+            '1.0.0~1-alpha.2',
             '1.0.0',
         ],
         '==0.1.2': [
             '0.1.2+build42-12.2012-01-01.12h23',
+        ],
+        '==0.1.2~2': [
+            '0.1.2~2',
         ],
         '=0.1.2': [
             '0.1.2+build42-12.2012-01-01.12h23',
@@ -65,33 +73,51 @@ class MatchTestCase(unittest.TestCase):
         ],
         '<=0.1.2': [
             '0.1.1',
+            '0.1.1~2',
             '0.1.2-rc1',
             '0.1.2-rc1.3.4',
             '0.1.2',
             '0.1.2+build4',
         ],
+        '<=0.1.2~3': [
+            '0.1.1',
+            '0.1.1~2',
+            '0.1.2',
+            '0.1.2~2',
+            '0.1.2~3',
+        ],
+        '>=0.1.2~3': [
+            '0.1.2~3',
+            '0.1.2~4',
+            '0.1.3',
+        ],
         '!=0.1.2+': [
             '0.1.2+1',
+            '0.1.2~2',
             '0.1.2-rc1',
         ],
         '!=0.1.2-': [
             '0.1.1',
+            '0.1.2~2',
             '0.1.2-rc1',
         ],
         '!=0.1.2+345': [
             '0.1.1',
+            '0.1.1~2+345',
             '0.1.2-rc1+345',
             '0.1.2+346',
             '0.2.3+345',
         ],
         '>=0.1.1': [
             '0.1.1',
+            '0.1.1~2',
             '0.1.1+build4.5',
             '0.1.2-rc1.3',
             '0.2.0',
             '1.0.0',
         ],
         '>0.1.1': [
+            '0.1.2~2',
             '0.1.2+build4.5',
             '0.1.2-rc1.3',
             '0.2.0',
@@ -104,22 +130,26 @@ class MatchTestCase(unittest.TestCase):
         ],
         '^0.1.2': [
             '0.1.2',
+            '0.1.2~2',
             '0.1.2+build4.5',
             '0.1.3-rc1.3',
             '0.1.4',
         ],
         '~0.1.2': [
             '0.1.2',
+            '0.1.2~2',
             '0.1.2+build4.5',
             '0.1.3-rc1.3',
         ],
         '~=1.4.5': (
             '1.4.5',
+            '1.4.8~2',
             '1.4.10-alpha',
             '1.4.10',
         ),
         '~=1.4': [
             '1.4.0',
+            '1.4.0~2',
             '1.6.10-alpha',
             '1.6.10',
         ],

tests/test_manifest.py
@@ -14,7 +14,7 @@ def test_manifest_hash(valid_manifest, valid_manifest_hash):
 
 def test_project_manifest_builder(valid_manifest):
     manifest = Manifest.fromdict(valid_manifest, name='test')
-    assert str(manifest.version) == '2.3.1'
+    assert str(manifest.version) == '2.3.1~2'
     assert manifest.description == 'Test project'
     assert len(manifest.dependencies) == 8
     assert manifest.targets == ['esp32']
@@ -50,4 +50,4 @@ def test_validator_valid_manifest(valid_manifest):
 def test_validator_passed_version(valid_manifest):
     errors = ManifestValidator(valid_manifest, version='5.0.0').validate_normalize()
     assert len(errors) == 1
-    assert 'Manifest version (2.3.1) does not match the version specified in the command line (5.0.0).' in errors[0]
+    assert 'Manifest version (2.3.1~2) does not match the version specified in the command line (5.0.0).' in errors[0]

commit 6340630d8167503b31676090a477297183bbdba2

fix: version string when revision number is 1
this is a regression from commit 6d09618498571dc958f745edb6706ea13eea1f1c

.pre-commit-config.yaml
@@ -53,4 +53,5 @@ repos:
           - thefuzz[speedup] == 0.19.0
         language: python
         files: \.(py|c|h)$
+        exclude: ^idf_component_tools/semver/.*|^tests/semver/.*
         require_serial: true

idf_component_tools/semver/base.py
@@ -315,7 +315,7 @@ class Version(object):
             version = '%s.%d' % (version, self.minor)
         if self.patch is not None:
             version = '%s.%d' % (version, self.patch)
-        if self.revision > 1:
+        if self.revision:
             version = '%s~%d' % (version, self.revision)
         if self.prerelease:
             version = '%s-%s' % (version, '.'.join(self.prerelease))

tests/semver/test_base.py
@@ -114,6 +114,7 @@ class VersionTestCase(unittest.TestCase):
         '1.0.0-rc.1': (1, 0, 0, 0, ('rc', '1'), ()),
         '1.0.0-rc.1+build.1': (1, 0, 0, 0, ('rc', '1'), ('build', '1')),
         '1.0.0': (1, 0, 0, 0, (), ()),
+        '1.0.0~1': (1, 0, 0, 1, (), ()),
         '1.0.0~99': (1, 0, 0, 99, (), ()),
         '1.0.0+0.3.7': (1, 0, 0, 0, (), ('0', '3', '7')),
         '1.3.7+build': (1, 3, 7, 0, (), ('build', )),