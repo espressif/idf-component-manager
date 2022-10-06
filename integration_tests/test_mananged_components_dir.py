47cbf48286115cf3960717233ebb9c50803548c7
Sergei Silnov <sergei.silnov@espressif.com>
2022-10-06 16:28:17 +0200

    Reorganize integration tests

---
+++ integration_tests/test_mananged_components_dir.py
@@ -0,0 +1,76 @@
+# SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
+# SPDX-License-Identifier: Apache-2.0
+import os
+import shutil
+from io import open
+from pathlib import Path
+
+import pytest
+
+from .integration_test_helpers import project_action
+
+
+@pytest.mark.parametrize(
+    'project', [
+        {
+            'components': {
+                'main': {
+                    'dependencies': {
+                        'example/cmp': {
+                            'version': '>=3.3.5',
+                        },
+                    }
+                }
+            }
+        },
+    ],
+    indirect=True)
+def test_changes_in_component(project):
+    res = project_action(project, 'reconfigure')
+    assert 'Build files have been written to' in res
+
+    with open(os.path.join(project, 'managed_components', 'example__cmp', 'README.md'), 'w') as f:
+        f.write(u'TEST STRING')
+    shutil.rmtree(os.path.join(project, 'build'))
+    res = project_action(project, 'reconfigure')
+
+    assert 'in the "managed_components" directory were modified' in res
+
+    shutil.move(
+        os.path.join(project, 'managed_components', 'example__cmp'),
+        os.path.join(project, 'components', 'example__cmp'))
+    res = project_action(project, 'reconfigure')
+
+    assert 'Build files have been written to' in res
+
+    shutil.move(os.path.join(project, 'components', 'example__cmp'), os.path.join(project, 'components', 'cmp'))
+    res = project_action(project, 'reconfigure')
+
+    assert 'Build files have been written to' in res
+
+
+@pytest.mark.parametrize(
+    'project', [
+        {
+            'components': {
+                'main': {
+                    'dependencies': {
+                        'example/cmp': {
+                            'version': '^3.3.0',
+                        }
+                    }
+                }
+            }
+        },
+    ], indirect=True)
+def test_fullclean_managed_components(project):
+    project_action(project, 'reconfigure')
+    assert Path(project, 'managed_components').is_dir()
+    project_action(project, 'fullclean')
+    assert not Path(project, 'managed_components').is_dir()
+    project_action(project, 'reconfigure')
+    component_hash = Path(project, 'managed_components', 'example__cmp', '.component_hash')
+    with component_hash.open(mode='wt') as hash_file:
+        hash_file.write(u'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855')
+    project_action(project, 'fullclean')
+    assert Path(project, 'managed_components', 'example__cmp').is_dir()
---
+++ integration_tests/test_targets.py
@@ -0,0 +1,100 @@
+# SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
+# SPDX-License-Identifier: Apache-2.0
+import os
+from io import open
+
+import pytest
+
+from .integration_test_helpers import set_target, skip_for_idf_versions
+
+
+@pytest.mark.parametrize(
+    'project', [
+        {
+            'components': {
+                'main': {
+                    'dependencies': {
+                        'example/cmp': {
+                            'version': '>=3.3.5',
+                        }
+                    }
+                }
+            }
+        },
+    ], indirect=True)
+def test_changing_target(project):
+    if skip_for_idf_versions('v4.1'):
+        return
+
+    lock_path = os.path.join(project, 'dependencies.lock')
+    res = set_target(project, 'esp32')
+    assert 'Building ESP-IDF components for target esp32' in res
+    with open(lock_path, mode='r', encoding='utf-8') as f:
+        assert 'esp32\n' in f.read()
+    res = set_target(project, 'esp32s2')
+    assert 'Building ESP-IDF components for target esp32s2' in res
+    with open(lock_path, mode='r', encoding='utf-8') as f:
+        assert 'esp32s2\n' in f.read()
+
+
+@pytest.mark.parametrize(
+    'project', [
+        {
+            'components': {
+                'main': {
+                    'dependencies': {
+                        'idf': {
+                            'version': '>=4.1.0',
+                        }
+                    },
+                    'targets': ['esp32s2'],
+                }
+            },
+        },
+    ],
+    indirect=True)
+def test_idf_check_target_fail_manifest(project):
+    res = set_target(project, 'esp32')
+    assert 'Component "main" does not support target esp32' in res
+
+
+@pytest.mark.parametrize(
+    'project', [
+        {
+            'components': {
+                'main': {
+                    'dependencies': {
+                        'example/cmp': {
+                            'version': '0.0.1',
+                        },
+                    }
+                }
+            }
+        },
+    ], indirect=True)
+def test_idf_check_target_fail_dependency(project):
+    res = set_target(project, 'esp32')
+    assert 'project depends on example/cmp (0.0.1) which doesn\'t match' in res
+
+
+@pytest.mark.parametrize(
+    'project', [
+        {
+            'components': {
+                'main': {
+                    'dependencies': {
+                        'idf': {
+                            'version': '>=4.1.0',
+                        },
+                        'example/cmp': {
+                            'version': '3.3.3',
+                        },
+                    }
+                }
+            }
+        },
+    ],
+    indirect=True)
+def test_idf_check_target_pass(project):
+    res = set_target(project, 'esp32')
+    assert 'Build files have been written to:' in res
