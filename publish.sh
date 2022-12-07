39c81d4763a853cf3558d5b9fcd7717689963927
Sergei Silnov <sergei.silnov@espressif.com>
2022-12-07 23:14:44 +0100

    CI: check all existing versions during upload to pypi

--- publish.sh
+++ publish.sh
@@ -1,14 +1,16 @@
 #! /bin/bash
 set -eo pipefail
 
-PUBLISHED_VERSION=$(curl https://pypi.org/pypi/idf-component-manager/json 2>/dev/null | jq -r '.info.version')
 CURRENT_VERSION=$(python setup.py --version 2>/dev/null)
 
-if [ "$PUBLISHED_VERSION" == "$CURRENT_VERSION" ]; then
-  echo "Version ${PUBLISHED_VERSION} already published, skipping..."
-else
-  echo "Packaging and publishing new version: ${CURRENT_VERSION}"
-  rm -rf dist
-  python setup.py sdist bdist_wheel
-  twine upload dist/*
-fi
+while IFS='' read -r PUBLISHED_VERSION; do
+  if  [ "${PUBLISHED_VERSION}" == "${CURRENT_VERSION}" ] ; then
+    echo "Version ${CURRENT_VERSION} already published, skipping..."
+    exit 0
+  fi
+done < <(curl https://pypi.org/pypi/idf-component-manager/json 2>/dev/null | jq -r '.releases | keys[]')
+
+echo "Packaging and publishing new version: ${CURRENT_VERSION}"
+rm -rf dist
+python setup.py sdist bdist_wheel
+twine upload dist/*
