7c7950fbbf01fb7a1ba8087dd0b329c02cb81f94
Sergei Silnov <sergei.silnov@espressif.com>
2023-01-17 21:40:08 +0100

    Bump version to v1.2.2

--- idf_component_tools/__version__.py
+++ idf_component_tools/__version__.py
@@ -1,5 +1,5 @@
-# SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
+# SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
 # SPDX-License-Identifier: Apache-2.0
 """IDF Component Tools Version"""
 
-__version__ = '1.2.1'
+__version__ = '1.2.2'
