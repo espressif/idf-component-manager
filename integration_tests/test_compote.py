f92fcf7c8386582d87c194569ee2f80b485e0811
Sergei Silnov <sergei.silnov@espressif.com>
2023-01-17 15:55:54 +0100

    CI: Increase timeout for completion tests

--- integration_tests/test_compote.py
+++ integration_tests/test_compote.py
@@ -22,21 +22,22 @@ import pytest
         'bash',
         'zsh',
     ])
-@pytest.mark.flaky(reruns=5, reruns_delay=2)
+@pytest.mark.flaky(reruns=10, reruns_delay=2)
 def test_autocomplete(shell, monkeypatch):
     if shell in ['fish']:
         monkeypatch.setenv('TERM', 'screen-256color')  # var TERM is required in fish
 
     with open(os.path.join(os.path.dirname(__file__), '..', '{}.txt'.format(shell)), 'wb') as fw:
+        TIMEOUT = 10
         # install autocomplete
         child = pexpect.spawn('{} -i'.format(shell))
         child.logfile = fw
-        child.expect([r'\$ ', '# ', '> '], timeout=5)
+        child.expect([r'\$ ', '# ', '> '], timeout=TIMEOUT)
         child.sendline('compote autocomplete --shell {}'.format(shell))
         # test autocomplete
-        child.expect([r'\$ ', '# ', '> '], timeout=5)
+        child.expect([r'\$ ', '# ', '> '], timeout=TIMEOUT)
         child.sendline('exec {}'.format(shell))  # reload
-        child.expect([r'\$ ', '# ', '> '], timeout=5)
+        child.expect([r'\$ ', '# ', '> '], timeout=TIMEOUT)
         child.send('compote \t\t')
         for group in ['autocomplete', 'component', 'manifest', 'project']:
-            child.expect(group, timeout=1)
+            child.expect(group, timeout=TIMEOUT / 2)
