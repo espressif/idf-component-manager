2d8733c7c1572f6439a33213d11a9c9ea37d5288
Sergei Silnov <sergei.silnov@espressif.com>
2023-01-18 23:03:41 +0100

    Add sphinx documentation template

---
+++ docs/conf_common.py
@@ -0,0 +1,36 @@
+# SPDX-FileCopyrightText: 2023 Espressif Systems (Shanghai) CO LTD
+# SPDX-License-Identifier: Apache-2.0
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+from idf_component_tools.__version__ import __version__
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+version = __version__
+project_homepage = 'https://components.espressif.com/'
+
+author = 'Espressif Systems (Shanghai) Co., Ltd.'
+languages = ['en']
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx_rtd_theme', 'sphinx_copybutton', 'sphinx_tabs.tabs']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# Directories are relative to the language directory
+static_path = ['../_static']
+html_theme = 'sphinx_rtd_theme'
+html_logo = '../_static/logo.png'
+html_favicon = '../_static/favicon.ico'
+html_static_path = ['../_static']
+html_css_files = ['theme_overrides.css']
---
+++ docs/en/index.rst
@@ -0,0 +1,13 @@
+IDF Component Manager and ESP Component Registry Documentation
+===============================================================
+
+The IDF Component Manager is a tool that allows developers to manage components for the ESP-IDF development framework. The tool is compatible with ESP-IDF versions 4.1 and later, and is included by default starting with ESP-IDF version 4.4.
+
+The ESP Component Registry is a central repository for components that can be used with the ESP-IDF. It is hosted at https://components.espressif.com and provides a convenient way for developers to discover and download components for their projects.
+
+With the IDF Component Manager, developers can easily install components from the ESP-IDF Component Registry, streamlining the process of adding new functionality to their projects.
+
+.. toctree::
+   :caption: Contents:
+
+   Getting Started <getting_started>
