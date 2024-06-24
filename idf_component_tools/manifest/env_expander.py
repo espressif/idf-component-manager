227f7b424380e5a47f04e0f4d2cbc936821454ed
Fu Hanxi <fuhanxi@espressif.com>
2024-06-24 15:24:19 +0800

    fix: assume false when exceptions raised in if clause

--- idf_component_tools/manifest/env_expander.py
+++ idf_component_tools/manifest/env_expander.py
@@ -1,17 +1,21 @@
 # SPDX-FileCopyrightText: 2022-2024 Espressif Systems (Shanghai) CO LTD
 # SPDX-License-Identifier: Apache-2.0
 
+import os
 import typing as t
 from string import Template
 
-from idf_component_tools.errors import ManifestError
+from idf_component_tools.errors import ManifestError, RunningEnvironmentError
 
 
-def subst_vars_in_str(s: str, env: t.Dict[str, t.Any]) -> str:
+def subst_vars_in_str(s: str, env: t.Dict[str, t.Any] = None) -> str:  # type: ignore
+    if env is None:
+        env = os.environ
+
     try:
         return Template(s).substitute(env)
     except KeyError as e:
-        raise ManifestError(f'Environment variable "{e.args[0]}" is not set')
+        raise RunningEnvironmentError(f'Environment variable "{e.args[0]}" is not set')
     except ValueError:
         raise ManifestError(
             'Invalid format of environment variable in the value: "{}".\n'
--- tests/manifest/test_env_expander.py
+++ tests/manifest/test_env_expander.py
@@ -3,10 +3,11 @@
 
 import pytest
 
-from idf_component_tools.errors import ManifestError
+from idf_component_tools.errors import ManifestError, RunningEnvironmentError
 from idf_component_tools.manifest.env_expander import (
     subst_vars_in_str,
 )
+from idf_component_tools.manifest.if_parser import parse_if_clause
 
 TEST_ENVIRON = {
     'A': '',
@@ -28,12 +29,51 @@ def test_expand_env_vars_in_str_ok(inp, env, exp):
 
 
 @pytest.mark.parametrize(
-    'inp,env,err',
+    'inp,env',
     [
-        ('$100', TEST_ENVIRON, 'Invalid'),
-        ('$A100', TEST_ENVIRON, 'not set'),
+        ('$A100', TEST_ENVIRON),
+        ('$ABC', TEST_ENVIRON),
     ],
 )
-def test_expand_env_vars_in_str_err(inp, env, err):
-    with pytest.raises(ManifestError, match=err):
+def test_expand_env_vars_in_str_env_not_set_err(inp, env):
+    with pytest.raises(RunningEnvironmentError, match='Environment variable.+is not set'):
         subst_vars_in_str(inp, env)
+
+
+@pytest.mark.parametrize(
+    'inp,env',
+    [
+        ('$100', TEST_ENVIRON),
+        ('${A', TEST_ENVIRON),
+    ],
+)
+def test_expand_env_vars_in_str_env_format_err(inp, env):
+    with pytest.raises(
+        ManifestError, match='Invalid format of environment variable in the value.+'
+    ):
+        subst_vars_in_str(inp, env)
+
+
+@pytest.mark.parametrize(
+    'if_clause',
+    [
+        'target == esp32 && ${FOO} in [ "FOO" ]',
+        '${FOO}_BAR == "FOO_BAR"',
+        '${VER} < 2.11',
+    ],
+)
+def test_expand_env_vars_with_optional_dependencies(if_clause, monkeypatch):
+    monkeypatch.setenv('IDF_TARGET', 'esp32')
+
+    clause = parse_if_clause(if_clause)
+
+    monkeypatch.setenv('FOO', 'FOO')
+    monkeypatch.setenv('VER', '2.9.0')
+    assert clause.get_value() is True
+
+    monkeypatch.delenv('FOO')
+    monkeypatch.delenv('VER')
+    with pytest.warns(
+        UserWarning, match='Environment variable.+is not set, assume the condition is False'
+    ):
+        assert clause.get_value() is False
