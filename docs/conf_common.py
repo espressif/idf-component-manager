2892fb6834b230ec56223c1b23586665f1adcded
Fu Hanxi <fuhanxi@espressif.com>
2024-08-29 11:52:25 +0200

    docs: replace DEFAULT_EXCLUDE to runtime value in docs

--- docs/conf_common.py
+++ docs/conf_common.py
@@ -5,7 +5,9 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+
 from idf_component_tools.__version__ import __version__
+from idf_component_tools.file_tools import DEFAULT_EXCLUDE
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -44,3 +46,19 @@ html_logo = '../_static/logo.png'
 html_favicon = '../_static/favicon.ico'
 html_static_path = ['../_static']
 html_css_files = ['theme_overrides.css']
+
+
+def _list_of_strings_to_html(list_of_strings):
+    s = '<ul>'
+    for string in sorted(list_of_strings):
+        s += f'<li><code class="python literal">{string}</code></li>'
+    s += '</ul>'
+    return s
+
+
+# Dynamic Values
+rst_prolog = f"""
+.. |DEFAULT_EXCLUDE| raw:: html
+
+   {_list_of_strings_to_html(DEFAULT_EXCLUDE)}
+"""
