d54dfc566d523b9d832cdefbe2969797decf675c
Sergei Silnov <sergei.silnov@espressif.com>
2025-12-16 19:37:14 +0100

    ci: add way to integration tests against given ESP-IDF commit/branch

--- ci/prepare_tests.py
+++ ci/prepare_tests.py
@@ -1,12 +1,37 @@
-# SPDX-FileCopyrightText: 2022-2024 Espressif Systems (Shanghai) CO LTD
+# SPDX-FileCopyrightText: 2022-2025 Espressif Systems (Shanghai) CO LTD
 # SPDX-License-Identifier: Apache-2.0
 
 import re
-import shutil
 import subprocess
-from io import open
 from os import environ, getenv
 from pathlib import Path
+
+from jinja2 import Environment, FileSystemLoader
+
+# Default template configuration
+DEFAULT_IDF_BRANCHES = ['release-v6.0', 'master']
+DEFAULT_PYTHON_VERSIONS = ['3.10', '3.14']
+DEFAULT_PYTEST_SPLIT_GROUPS = [1, 2, 3, 4, 5]
+
+# Constraint file version mapping for master branch
+MASTER_CONSTRAINT_VERSION = 'v6.1'
+
+
+def get_template_config() -> dict:
+    """Get template configuration, allowing env var overrides."""
+    idf_branches_env = getenv('IDF_BRANCHES')
+    if idf_branches_env:
+        idf_branches = [b.strip() for b in idf_branches_env.split(',')]
+    else:
+        idf_branches = DEFAULT_IDF_BRANCHES
+
+    return {
+        'idf_branches': idf_branches,
+        'python_versions': DEFAULT_PYTHON_VERSIONS,
+        'pytest_split_groups': DEFAULT_PYTEST_SPLIT_GROUPS,
+        'master_constraint_version': MASTER_CONSTRAINT_VERSION,
+    }
+
 
 # integration tests
 INTEGRATION_TESTS_RE = re.compile(r'^(.*,)*run_integration_tests(,.*)*$')
@@ -80,16 +105,24 @@ def should_run_integration_tests() -> bool:
     return False
 
 
+def render_template(ci_dir: Path, template_name: str, output_file) -> None:
+    """Render a Jinja2 template and append to output file."""
+    env = Environment(loader=FileSystemLoader(ci_dir))
+    template = env.get_template(template_name)
+    rendered = template.render(**get_template_config())
+    output_file.write(rendered)
+    output_file.write('\n')
+
+
 def main():
     ci_dir = Path(environ['CI_PROJECT_DIR']) / 'ci'
-    if should_run_integration_tests():
-        print('Adding integration tests')
-        with open(ci_dir / 'tests.yml', 'a') as out, open(ci_dir / 'integration_tests.yml') as inp:
-            shutil.copyfileobj(inp, out)
-    if should_run_build_docker_files():
-        print('Adding build docker files')
-        with open(ci_dir / 'tests.yml', 'a') as out, open(ci_dir / 'build_docker.yml') as inp:
-            shutil.copyfileobj(inp, out)
+    with open(ci_dir / 'tests.yml', 'a') as out:
+        if should_run_integration_tests():
+            print('Adding integration tests')
+            render_template(ci_dir, 'integration_tests.yml.j2', out)
+        if should_run_build_docker_files():
+            print('Adding build docker files')
+            render_template(ci_dir, 'build_docker.yml.j2', out)
 
 
 if __name__ == '__main__':
